48588e62f4d783e8595866f2a89fc2a2afa8778a
malteostling <malte.ostling@elev.ga.ntig.se>
2025-03-09 19:40:40 +0100

    ändrar symbolen i nav baren

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
-document.querySelector(".fade-layer").addEventListener("click",ShowMenu)
+document.querySelector(".fade-layer").addEventListener("click", ShowMenu)
 
-document.querySelector(".menu-button").addEventListener("click",ShowMenu)
+document.querySelector(".menu-button").addEventListener("click", ShowMenu)
+document.querySelector(".close").addEventListener("click", ShowMenu)
 
-document.querySelector(".close-button").addEventListener("click",ShowMenu)
 
-function ShowMenu(){
-    let menu = document.querySelector("div.menu")
-    menu.classList.toggle("show")
-    close.classList.toggle("show")
+function ShowMenu() {
 
     let close = document.querySelector("div.close")
-    layer.classList.toggle("visible")
+    let button = document.querySelector("div.menu-button")
+
+    let menu = document.querySelector("div.menu")
+    close.classList.toggle("visible"), menu.classList.toggle("show"), button.classList.toggle("invisible")
 
     let layer = document.querySelector(".fade-layer")
-    layer.classList.toggle("visible")
+    layer.classList.toggle("visible")  
+
+
+  
+
+   
 }
