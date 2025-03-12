83063b1ceeb9fe5c94189dc11ac7c2b53e9b3f84
malteostling <malte.ostling@elev.ga.ntig.se>
2025-03-12 11:15:14 +0100

    Lagt till en tabel och sortering

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ document.querySelector(".fade-layer").addEventListener("click", ShowMenu)
 
 document.querySelector(".menu-button").addEventListener("click", ShowMenu)
 document.querySelector(".close").addEventListener("click", ShowMenu)
+document.querySelector(".button1").addEventListener("click", Sortpris)
+document.querySelector(".button2").addEventListener("click", Sortkpk)
+document.querySelector(".button3").addEventListener("click", Sortkoffin)
 
 
 function ShowMenu() {
@@ -14,9 +17,78 @@ function ShowMenu() {
 
     let layer = document.querySelector(".fade-layer")
     layer.classList.toggle("visible")  
+}
 
 
-  
+function Sortpris() {
+ /*Tagen från w3schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_number*/
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("Table");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[2];
+          y = rows[i + 1].getElementsByTagName("TD")[2];
+          if (Number(x.innerHTML) > Number(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+        }
+      }
 
-   
+}
+function Sortkpk() {
+    /*Tagen från w3schools men ändrad så att den kollar på högsta tallet https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_number */
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("Table");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[3];
+          y = rows[i + 1].getElementsByTagName("TD")[3];
+          if (Number(x.innerHTML) < Number(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+        }
+      }
+}
+
+function Sortkoffin() {
+     /*Tagen från w3schools men ändrad så att den kollar på högsta tallet https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_number */
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("Table");
+    switching = true;
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+          shouldSwitch = false;
+          x = rows[i].getElementsByTagName("TD")[1];
+          y = rows[i + 1].getElementsByTagName("TD")[1];
+          if (Number(x.innerHTML) < Number(y.innerHTML)) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+        if (shouldSwitch) {
+          rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+          switching = true;
+        }
+      }
+
 }
